6fa6129956addf9d71362d715a837df62f7ed6f8
Kenny Deblaere <kenny.deblaere@live.be>
2015-05-30 09:51:10 +0200

    Template, Listview, Slider & button

---
+++ serve.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Kenny Deblaere on 30/05/2015.
+ */
+var connect = require('connect');
+var serveStatic = require('serve-static');
+
+// we put our server serve.js in the same directory as
+// the html, css and js files we are going to serve.
+//
+// __dirname is a global variable that contains the directory
+// where the current running script (serve.js) resides.
+connect().use(serveStatic(__dirname)).listen(8080);
